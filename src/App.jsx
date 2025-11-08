f39364e830ff986b4f411496f34b63d59e4a5efa
Dev K Sarthi <sarthi.dev07@gmail.com>
2025-11-08 11:35:23 -0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import PhaserGame from "./PhaserGame.jsx";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          textAlign: "center",
+          background: "#000",
+          color: "#0f0",
+          height: "100vh",
+        }}
+      >
+        <h1>LEVEL 6-7</h1>
+        <PhaserGame />
+      </div>
+    </>
+  );
+}
+
+export default App;
